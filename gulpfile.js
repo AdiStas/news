fa3b0724c02efa271161eba47e552d8ff2432fe7
Stanislav <45733236+AdiStas@users.noreply.github.com>
2020-04-24 21:54:07 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var csso = require('gulp-csso');
+var rename = require("gulp-rename");
+
+gulp.task('style', function () { 
+	return gulp.src("./*.scss") 
+		.pipe(plumber())
+		.pipe(sass()) 
+		.pipe(postcss([
+			autoprefixer()
+		]))
+		.pipe(gulp.dest('./build/css')) 
+		.pipe(browserSync.stream())
+		.pipe(csso())
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('serve', gulp.series('style', function() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./**/*.scss", gulp.series('style'));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}));
